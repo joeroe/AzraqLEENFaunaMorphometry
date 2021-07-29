b871514e09ea57a3b6842a9fc54543df85c6efbd
Joe Roe <joe@joeroe.io>
2021-07-29 14:01:22 +0200

    Up MC simulation repetitions

--- analysis/2-montecarlo.R
+++ analysis/2-montecarlo.R
@@ -2,6 +2,6 @@
 
 # Assign an absolute age to each specimen by repeatedly sampling from the site
 # radiocarbon distribution
-gazella_sim <- sim_ages(gazella, radiocarbon_sum, SiteCode, 1000, Element, GLI, Bd, LSI, Z)
-lepus_sim <- sim_ages(lepus, radiocarbon_sum, SiteCode, 1000, Z)
-vulpes_sim <- sim_ages(vulpes, radiocarbon_sum, SiteCode, 1000, Z)
+gazella_sim <- sim_ages(gazella, radiocarbon_sum, SiteCode, 10000, Element, GLI, Bd, LSI, Z)
+lepus_sim <- sim_ages(lepus, radiocarbon_sum, SiteCode, 10000, Z)
+vulpes_sim <- sim_ages(vulpes, radiocarbon_sum, SiteCode, 10000, Z)
